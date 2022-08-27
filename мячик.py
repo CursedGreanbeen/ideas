44238a7aaa2fd426c4784b00c62a3858c4769bcf
CursedGreanbeen <101968163+CursedGreanbeen@users.noreply.github.com>
2022-08-27 23:31:19 +0500

    boundaries

--- мячик.py
+++ мячик.py
@@ -1,5 +1,6 @@
 from tkinter import *
 
+
 root = Tk()
 c = Canvas(width=300, height=300,
            bg='white')
@@ -9,12 +10,14 @@ c.pack()
 ball = c.create_oval(140, 140, 160, 160,
                      fill='green')
 c.bind('<Up>',
-       lambda event: c.move(ball, 0, -4))
+       lambda event: c.move(ball, 0, -4) if c.coords(ball)[1] >= 4 else False)
 c.bind('<Down>',
-       lambda event: c.move(ball, 0, 4))
+       lambda event: c.move(ball, 0, 4) if c.coords(ball)[3] <= 300 else False)
 c.bind('<Left>',
-       lambda event: c.move(ball, -4, 0))
+       lambda event: c.move(ball, -4, 0) if c.coords(ball)[0] >= 4 else False)
 c.bind('<Right>',
-       lambda event: c.move(ball, 4, 0))
+       lambda event: c.move(ball, 4, 0) if c.coords(ball)[2] <= 300 else False)
+
+
 
 root.mainloop()
